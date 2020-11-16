1403f92542e346333805fda2e4b08ed6d90db833
itachi <enchantinggg4@gmail.com>
2020-11-16 04:03:53 +0300

    test

--- process/match_created.py
+++ process/match_created.py
@@ -24,11 +24,15 @@
 #     public readonly info: MatchInfo,
 #   ) {}
 # }
+from threading import Timer
 
 from aioredis import Redis
 
 from gs.run_server import run_server
 from config.servers import find_server
+
+async def game_server_started(redis_queue: Redis, name, evt):
+    await redis_queue.publish_json(name, evt)
 
 
 async def process_match_created_event(redis_queue: Redis, evt):
@@ -41,11 +45,13 @@ async def process_match_created_event(redis_queue: Redis, evt):
         if good:
             server['down_for'] = 0
             server.pop('down_since', None)
-            await redis_queue.publish_json('GameServerStartedEvent', ({
+
+
+            Timer(5.0, game_server_started, (redis_queue, 'GameServerStartedEvent', {
                 'matchId': evt['matchId'],
                 'info': evt['info'],
                 'url': ip
-            }))
+            })).start()
         else:
             await redis_queue.publish_json('GameServerNotStartedEvent', ({
                 'matchId': evt['matchId'],
