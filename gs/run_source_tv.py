8a34bd52054c6b835d873cb433d3d17443cc08a1
itachi <enchantinggg4@gmail.com>
2020-11-21 11:14:10 +0300

    f

--- gs/run_source_tv.py
+++ gs/run_source_tv.py
@@ -5,6 +5,7 @@ import subprocess
 def run_sourcetv_relay(main_process: subprocess.Popen, path: str, game_port: int):
     relay_port = game_port + 5
     tv_port = game_port + 4
+    print("%d port of source tv", relay_port)
     cmd = "%s/srcds.exe -game dota -console +sv_hibernate_when_empty 1 +tv_port %d +tv_relay 127.0.0.1:%d +tv_relay_secret_code 0" % (
         path, relay_port, tv_port)
 
