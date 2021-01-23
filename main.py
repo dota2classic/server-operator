c6e380114012c890aea39d64f6d95ab8a454f095
itachi <enchantinggg4@gmail.com>
2021-01-23 23:51:34 +0300

    TEST

--- main.py
+++ main.py
@@ -180,14 +180,14 @@ async def checks(redis_queue):
 
 async def start():
     redis_queue = await aioredis.create_redis_pool('redis://%s:%d' % (REDIS_HOST, REDIS_PORT), password=REDIS_PASSWORD)
-    asyncio.create_task(handle_launch_command(redis_queue))
+    asyncio.ensure_future(handle_launch_command(redis_queue))
 
-    asyncio.create_task(handle_actualization_requested(redis_queue))
-    # asyncio.create_task(handle_kill_requested(redis_queue))
+    asyncio.ensure_future(handle_actualization_requested(redis_queue))
+    # asyncio.ensure_future()(handle_kill_requested(redis_queue))
 
-    asyncio.create_task(checks(redis_queue))
-    asyncio.create_task(server_discovery(redis_queue))
-    asyncio.create_task(handle_discovery_requested(redis_queue))
+    asyncio.ensure_future(checks(redis_queue))
+    asyncio.ensure_future(server_discovery(redis_queue))
+    asyncio.ensure_future(handle_discovery_requested(redis_queue))
 
 
 asyncio.get_event_loop().create_task(start())
