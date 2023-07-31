9e6e1f619bba44de50ac6fd95aed4775bf007a8a
Yuri Stepin <enchantinggg4@gmail.com>
2023-07-31 20:43:50 +0300

    srctv

--- gs/run_source_tv.py
+++ gs/run_source_tv.py
@@ -1,15 +1,27 @@
 import subprocess
 
 
+def get_srcds_path():
+    import platform
+    if platform.system() == "Linux":
+        return "srcds.sh"
+
+    elif platform.system() == "Windows":
+        return "srcds.exe"
 
 def run_sourcetv_relay(main_process: subprocess.Popen, path: str, game_port: int):
     relay_port = game_port + 5
     tv_port = game_port + 4
-    print("%d port of source tv", relay_port)
-    cmd = "%s/srcds.exe -game dota -console +sv_hibernate_when_empty 1 +tv_port %d +tv_relay 127.0.0.1:%d +tv_relay_secret_code 0" % (
-        path, relay_port, tv_port)
 
-    relay_process = subprocess.Popen(cmd)
+    path = '%s/%s' % (path, get_srcds_path())
+    print("%d port of source tv", relay_port)
+    cmd = "-game dota -console +sv_hibernate_when_empty 1 +tv_port %d +tv_relay 127.0.0.1:%d +tv_relay_secret_code 0" % (relay_port, tv_port)
+
+
+
+    fullcmd = [path] + cmd.split(' ')
+
+    relay_process = subprocess.Popen(fullcmd)
     # after main process is finished kill relay
     main_process.communicate()
     relay_process.kill()
